b2471ed6e0c407c62cf1cb9313143b2cb7a4a26f
Ross0024 <123201329+Ross0024@users.noreply.github.com>
2023-03-10 23:44:11 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,53 @@ const convertButton = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 
 function convertToCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
+return (fahrenheit - 32) * 5 / 9;
 }
 
 function convertToFahrenheit(celsius) {
-  return celsius * 9 / 5 + 32;
+return celsius * 9 / 5 + 32;
 }
 
 function handleConvertButtonClick() {
-  const celsius = celsiusInput.value;
-  const fahrenheit = fahrenheitInput.value;
-  
-  if (celsius) {
-    const convertedTemperature = convertToFahrenheit(c
+const celsius = celsiusInput.value;
+const fahrenheit = fahrenheitInput.value;
+
+if (celsius) {
+const convertedTemperature = convertToFahrenheit(celsius);
+result.innerHTML = ${celsius}&deg;C = ${convertedTemperature}&deg;F;
+} else if (fahrenheit) {
+const convertedTemperature = convertToCelsius(fahrenheit);
+result.innerHTML = ${fahrenheit}&deg;F = ${convertedTemperature}&deg;C;
+} else {
+result.innerHTML = 'Please enter a temperature.';
+}
+}
+
+convertButton.addEventListener('click', handleConvertButtonClick);
+
+celsiusInput.addEventListener('input', function() {
+if (celsiusInput.value === '') {
+result.innerHTML = 'Please enter a temperature.';
+} else {
+const convertedTemperature = convertToFahrenheit(celsiusInput.value);
+result.innerHTML = ${celsiusInput.value}&deg;C = ${convertedTemperature}&deg;F;
+}
+});
+
+fahrenheitInput.addEventListener('input', function() {
+if (fahrenheitInput.value === '') {
+result.innerHTML = 'Please enter a temperature.';
+} else {
+const convertedTemperature = convertToCelsius(fahrenheitInput.value);
+result.innerHTML = ${fahrenheitInput.value}&deg;F = ${convertedTemperature}&deg;C;
+}
+});
+
+// Bonus: Clear the result text when the user clicks into either input field
+celsiusInput.addEventListener('click', function() {
+result.innerHTML = '';
+});
+
+fahrenheitInput.addEventListener('click', function() {
+result.innerHTML = '';
+});
